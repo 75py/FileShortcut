9d913d7a4e458ef4c3b0ebd7f8cdd22bb7ac4033
75py <dev.75py@gmail.com>
2018-01-05 19:01:07 +0900

    Fix bugs(in 2.1.0, NullPointerException is occured if icon is null) refs #3

--- app/src/main/java/com/nagopy/android/fileshortcut/CreatedShortcutListActivity.kt
+++ app/src/main/java/com/nagopy/android/fileshortcut/CreatedShortcutListActivity.kt
@@ -129,7 +129,11 @@ class CreatedShortcutListActivity : KodeinAppCompatActivity(), AdapterView.OnIte
          * @param bitmap
          * @return converting bitmap and return a string
          */
-        fun convertToString(bitmap: Bitmap): String {
+        fun convertToString(bitmap: Bitmap?): String? {
+            if (bitmap == null) {
+                return null
+            }
+
             val stream = ByteArrayOutputStream()
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
             val b = stream.toByteArray()
@@ -140,7 +144,11 @@ class CreatedShortcutListActivity : KodeinAppCompatActivity(), AdapterView.OnIte
          * @param encodedString
          * @return bitmap (from given string)
          */
-        fun convertToBitmap(encodedString: String): Bitmap? {
+        fun convertToBitmap(encodedString: String?): Bitmap? {
+            if (encodedString == null) {
+                return null
+            }
+
             return try {
                 val encodeByte = Base64.decode(encodedString, Base64.DEFAULT)
                 BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
