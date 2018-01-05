0b0da2e96c37f5ce5c02a6354903c55270600510
75py <dev.75py@gmail.com>
2018-01-05 12:22:39 +0900

    Use ShortcutManagerCompat and ShortcutInfoCompat, show a error message if Oreo and pinned shortcuts are not supported

--- app/src/main/java/com/nagopy/android/fileshortcut/ShortcutCreator.kt
+++ app/src/main/java/com/nagopy/android/fileshortcut/ShortcutCreator.kt
@@ -16,16 +16,14 @@
 
 package com.nagopy.android.fileshortcut
 
-import android.annotation.TargetApi
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.content.pm.ShortcutInfo
-import android.content.pm.ShortcutManager
 import android.graphics.Bitmap
-import android.graphics.drawable.Icon
 import android.os.Build
-import android.os.SystemClock
+import android.support.v4.content.pm.ShortcutInfoCompat
+import android.support.v4.content.pm.ShortcutManagerCompat
+import android.support.v4.graphics.drawable.IconCompat
+import android.widget.Toast
 import timber.log.Timber
 
 class ShortcutCreator {
@@ -37,69 +35,31 @@ class ShortcutCreator {
                , icon: Bitmap) {
         Timber.d("path: %s\nname: %s\nmimeType: %s", pathString, shortcutName, mimeType)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createApi26(activity, pathString, shortcutName, mimeType, icon)
-        } else {
-            createApi25(activity, pathString, shortcutName, mimeType, icon)
-        }
-        activity.finish()
-    }
-
-    @TargetApi(Build.VERSION_CODES.O)
-    fun createApi26(activity: Activity
-                    , pathString: String
-                    , shortcutName: String
-                    , mimeType: String
-                    , icon: Bitmap) {
         val shortcutIntent = Intent(activity, LaunchShortcutActivity::class.java)
                 .setAction(Intent.ACTION_MAIN)
                 .putExtra(LaunchShortcutActivity.EXTRA_PATH, pathString)
                 .putExtra(LaunchShortcutActivity.EXTRA_MIMETYPE, mimeType)
-
-        val shortcutManager = activity.getSystemService(Context.SHORTCUT_SERVICE) as ShortcutManager
-        val shortcutInfo = ShortcutInfo.Builder(activity, "id_" + SystemClock.currentThreadTimeMillis())
+        val shortcutInfo = ShortcutInfoCompat.Builder(activity, "id_" + System.currentTimeMillis())
+                .setLongLabel(shortcutName)
                 .setShortLabel(shortcutName)
-                .setIcon(Icon.createWithBitmap(icon))
+                .setIcon(IconCompat.createWithBitmap(icon))
                 .setIntent(shortcutIntent)
                 .build()
         if (activity.intent?.action == Intent.ACTION_CREATE_SHORTCUT) {
-            val shortcutResultIntent = shortcutManager.createShortcutResultIntent(shortcutInfo)
-            setResult(activity, shortcutResultIntent)
+            val shortcutResultIntent = ShortcutManagerCompat.createShortcutResultIntent(activity, shortcutInfo)
+            Timber.d("setResult %s", shortcutResultIntent)
+            activity.setResult(Activity.RESULT_OK, shortcutResultIntent)
+            activity.finish()
         } else {
-            shortcutManager.requestPinShortcut(shortcutInfo, null)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+                    && !ShortcutManagerCompat.isRequestPinShortcutSupported(activity)) {
+                // Oreo>= and pinned shortcuts are not supported
+                Toast.makeText(activity, R.string.msg_not_supported_pinned_shortcuts, Toast.LENGTH_LONG).show()
+            } else {
+                ShortcutManagerCompat.requestPinShortcut(activity, shortcutInfo, null)
+                activity.finish()
+            }
         }
-    }
-
-    fun createApi25(activity: Activity
-                    , pathString: String
-                    , shortcutName: String
-                    , mimeType: String
-                    , icon: Bitmap) {
-        val shortcutIntent = Intent(activity, LaunchShortcutActivity::class.java)
-                .putExtra(LaunchShortcutActivity.EXTRA_PATH, pathString)
-                .putExtra(LaunchShortcutActivity.EXTRA_MIMETYPE, mimeType)
-
-        val intent = Intent()
-                .putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent)
-                .putExtra(Intent.EXTRA_SHORTCUT_ICON, icon)
-                .putExtra(Intent.EXTRA_SHORTCUT_NAME, shortcutName)
-
-        if (activity.intent?.action == Intent.ACTION_CREATE_SHORTCUT) {
-            setResult(activity, intent)
-        } else {
-            sendInstallShortcutBroadcast(activity, intent)
-        }
-    }
-
-    fun setResult(activity: Activity, intent: Intent) {
-        Timber.d("setResult %s", intent)
-        activity.setResult(Activity.RESULT_OK, intent)
-    }
-
-    fun sendInstallShortcutBroadcast(activity: Activity, intent: Intent) {
-        Timber.d("sendInstallShortcutBroadcast %s", intent)
-        intent.action = "com.android.launcher.action.INSTALL_SHORTCUT"
-        activity.sendBroadcast(intent)
     }
 
 }
